e4642a5baa22705ec9be6568aff3b4ebede93292
Visakh Unni <visakhunni36@gmail.com>
2019-04-15 22:44:06 +0530

    added command file in command dir

---
+++ Command Pattern/command.py
@@ -0,0 +1,183 @@
+
+
+# the command pattern 
+
+
+class Order:
+	"""The Invoker Class"""
+
+	def __init__(self, placeOrederCmd, cancelOrderCmd):
+		self.__placeOrderCommand = placeOrederCmd
+		self.__cancelOrderCommand = cancelOrderCmd
+		
+
+	def placeOrder(self):
+		self.__placeOrderCommand.execute()
+		
+	def cancelOrder(self):
+		self.__cancelOrderCommand.execute()
+		
+
+
+class Supplier:
+	"""The Receiver Class"""
+
+	def confirmOrder(self):
+		print('The Order is Confiremed')
+		
+	def deleteOrder(self):
+		print('The Order is Cancelled')
+		
+
+
+class Command:
+	"""The Command Abstract Class"""
+
+	def __init__(self):
+		# customize for your need
+		pass
+
+	def execute(self):
+		# Override
+		pass
+
+
+
+class PlaceOrederCommand(Command):
+	"""The Command Class for placing the order"""
+
+	def __init__(self, supplier):
+		self.__supplier = supplier
+		
+	def execute(self):
+		self.__supplier.confirmOrder()
+		
+
+
+class CancelOrderCommand(Command):
+	"""The Command Class for cancelling the oreder"""
+
+	def __init__(self, supplier):
+		self.__supplier = supplier
+		
+	def execute(self):
+		self.__supplier.deleteOrder()
+		
+
+
+class Customer:
+	"""The Client Class"""
+	def __init__(self):
+		self.__service_provider = Supplier()
+		self.__placeOrder = PlaceOrederCommand(self.__service_provider)
+		self.__cancelOrder = CancelOrderCommand(self.__service_provider)
+		self.__order = Order(self.__placeOrder, self.__cancelOrder)
+
+	def order(self, cmd):
+		cmd = cmd.strip().upper()
+		try:
+			if cmd == "CONFIRM":
+				self.__placeOrder()
+			elif cmd == "CANCEL":
+				self.__cancelOrder()
+			else:
+				print('Invalid COMMAND, Please Enter CONFIRM or CANCEL')
+		except Exception, msg:
+			print('Exception occured : %s' %msg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
